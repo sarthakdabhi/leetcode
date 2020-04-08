9bfcfe7f34648a56de266f557fe59aec7b1d5ce2
RxPrep <apps@rxprep.com>
2020-04-07 18:35:10 -0700

    690

--- 690.js
+++ 690.js
@@ -1,35 +1,36 @@
-const employees = [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]];
+const employees = [[1, 5, [2, 3]], [2, 3, [1]], [3, 3, []]];
 const id = 1;
 
 // With recursion
-var GetImportance = function(employees, id) {
-	for (let employee of employees) {
-		if (id == employee[0])
-		{
-			let imp = employee[1];
-			if (employee[2].length == 0) return employee[1];
-			else {
-				for (let sub of employee[2])
-					imp += GetImportance(employees,sub);
-				return imp;
-			}
-		}
-	}
-};
+// var GetImportance = function(employees, id) {
+// 	for (let employee of employees) {
+// 		if (id == employee[0])
+// 		{
+// 			let imp = employee[1];
+// 			if (employee[2].length == 0) return employee[1];
+// 			else {
+// 				for (let sub of employee[2])
+// 					imp += GetImportance(employees,sub);
+// 				return imp;
+// 			}
+// 		}
+// 	}
+// };
 
 // Without recursion
 var GetImportance = function(employees, id) {
-	let imp = 0, chain = [id];
+	let imp = 0, chain = [id], done = [];
 	while (chain.length > 0) {
 		for (let employee of employees) {
 			if (chain[0] == employee[0]) {
-				chain.shift();
 				imp += employee[1];
-				chain = chain.concat(employee[2]);
+				done.push(chain[0]);
+				chain.shift();
+				chain = chain.concat(employee[2].filter(v => done.indexOf(v) == -1));
 			}
 		}
 	}
 	return imp;
 };
 
-console.log(GetImportance(employees, id));
+console.log(GetImportance([[1,2,[2]], [2,3,[1]]], 2));
